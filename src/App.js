cf51bbd143fff80b1bca2bd9f7ef46871f5ce439
nikitasangle <nikita.28994@kmagrawalcollege.in>
2022-01-07 21:15:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import React, { useState } from 'react';
+import './App.css';
+import MultipleInput from './Components/Form/Multipleinput';
+
+
+function App() {
+  return (
+    <div>
+      <MultipleInput/>
+
+    </div>
+  );
+}
+
+
+
+export default App;
